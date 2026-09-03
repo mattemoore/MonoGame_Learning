6255ecfd31ff4049370cb7cd4625e6301892b91b
mattemoore <matt.e.moore@gmail.com>
2026-09-02 20:34:44 -0400

    Consolidate sprite sheet loading into Core SpriteSheetAsset

--- MonoGameLearning.Game/AnimatedSprites/EnemySprite.cs
+++ MonoGameLearning.Game/AnimatedSprites/EnemySprite.cs
@@ -1,4 +1,3 @@
-using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using MonoGame.Extended.Graphics;
 using MonoGameLearning.Core.Rendering;
@@ -15,30 +14,17 @@ public static class EnemySprite
     public const string AnimationFall = "fall";
     public const string AnimationGetUp = "getup";
 
-    private static SpriteSheet _spriteSheet;
-    private static bool _loaded;
+    private static readonly SpriteSheetAsset Asset = new(
+        "adventurer", "images/adventurer",
+        new SpriteAnimationDef(AnimationIdle, "adventurer-idle", 4, true),
+        new SpriteAnimationDef(AnimationRun, "adventurer-run", 6, true),
+        new SpriteAnimationDef(AnimationAttack1, "adventurer-attack1", 4, false),
+        new SpriteAnimationDef(AnimationHurt, "adventurer-hurt", 3, false),
+        new SpriteAnimationDef(AnimationDie, "adventurer-die", 7, false),
+        new SpriteAnimationDef(AnimationFall, "adventurer-fall", 2, false),
+        new SpriteAnimationDef(AnimationGetUp, "adventurer-stand", 3, false));
 
-    public static void Load(ContentManager content)
-    {
-        if (_loaded) return;
-        _loaded = true;
+    public static void Load(ContentManager content) => Asset.Load(content);
 
-        Texture2DAtlas atlas = content.Load<Texture2DAtlas>("images/adventurer");
-        _spriteSheet = new("adventurer", atlas);
-
-        _spriteSheet.DefineFrames(AnimationIdle, "adventurer-idle", 4, true);
-        _spriteSheet.DefineFrames(AnimationRun, "adventurer-run", 6, true);
-        _spriteSheet.DefineFrames(AnimationAttack1, "adventurer-attack1", 4, false);
-        _spriteSheet.DefineFrames(AnimationHurt, "adventurer-hurt", 3, false);
-        _spriteSheet.DefineFrames(AnimationDie, "adventurer-die", 7, false);
-        _spriteSheet.DefineFrames(AnimationFall, "adventurer-fall", 2, false);
-        _spriteSheet.DefineFrames(AnimationGetUp, "adventurer-stand", 3, false);
-    }
-
-    public static AnimatedSprite Create()
-    {
-        var sprite = new AnimatedSprite(_spriteSheet, AnimationIdle);
-        sprite.Origin = new Vector2(sprite.Size.X / 2f, sprite.Size.Y / 2f);
-        return sprite;
-    }
+    public static AnimatedSprite Create() => Asset.Create(AnimationIdle);
 }
